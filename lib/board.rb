2dcd47ff9aa02608749369c4985103341775a95f
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-29 13:32:12 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -12,11 +12,10 @@ def reset!
 end
 
 def display
-  puts "-----------"
   puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-  puts "---------"
+  puts "-----------"
   puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-  puts "---------"
+  puts "-----------"
   puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
 end
 
