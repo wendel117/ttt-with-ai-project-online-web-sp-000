77b411a0bcae02df1817705953eea40c9cae0cca
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-29 15:42:26 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -45,8 +45,21 @@ def winner
 end
 
 def turn
-  
+  valid_move?
 end
+
+#def turn
+#  puts "Please choose a number 1-9:"
+#  user_input = gets.chomp
+#  index = input_to_index(user_input)
+#  if valid_move?(index)
+#    player_token = current_player
+#    move(index, player_token)
+#    display_board
+#  else
+#    turn
+#  end
+#end
 
 def play
 
