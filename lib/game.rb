23e53120159a1a41fe03ed8873a782ceac2b43b5
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-29 14:39:00 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -13,6 +13,9 @@ WIN_COMBINATIONS = [
 ]
 
 def initialize(player_1= Players::Human.new("X"), player_2=Players::Human.new("O"), board=board.new)
+  @board=board
+  @player_1=player_1
+  @player_2=player_2
 end
 
 
