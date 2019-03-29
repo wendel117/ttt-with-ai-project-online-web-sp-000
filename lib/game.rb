fe53f16dd1ad0925a52693e85e8d94465a4b22ab
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-29 15:25:25 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -26,7 +26,7 @@ def won?
   WIN_COMBINATIONS.detect do |winner|
     @board.cells[winner[0]] == @board.cells[winner[1]] &&
     @board.cells[winner[1]] == @board.cells[winner[2]] 
-
+  end
 end
 
 def draw?
