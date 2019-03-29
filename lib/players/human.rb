920c54fd3edbcfa84d5ca389cff938fdcdc0b6e3
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-29 14:25:57 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,11 +2,11 @@ module Players
   class Human < Player
 
     attr_accessor :board
-    
+
     def move(input)
       puts "Please enter your selection"
       gets.strip
-      
+
     end
 
   end
