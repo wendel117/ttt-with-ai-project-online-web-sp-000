e5a2cafcb171be3fa9b40c05c8119bd6ae3f9ac2
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-29 15:49:48 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -51,26 +51,10 @@ def turn
  end
 
  def play
-    banner
     turn until over?
     board.display
     puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
   end
-#def turn
-#  puts "Please choose a number 1-9:"
-#  user_input = gets.chomp
-#  index = input_to_index(user_input)
-#  if valid_move?(index)
-#    player_token = current_player
-#    move(index, player_token)
-#    display_board
-#  else
-#    turn
-#  end
-#end
 
-def play
-
-end
 
 end
